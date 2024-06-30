3786642e0a643b6fed942b0fe3584cc17523d2fb
Ningrui Zhang <ningrui1227@gmail.com>
2024-06-30 21:11:55 +0100

    add program_125.c

---
+++ program_125.c
@@ -0,0 +1,28 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int fibonacci(int k);
+
+int main(void)
+{
+    int n = get_int("Enter position: ");
+
+    int result = fibonacci(n);
+    printf("%d\n", result);
+    return 0;
+}
+
+int fibonacci(int k)
+{
+    if (k == 1)
+    {
+        return 0;
+    }
+
+    if (k == 2)
+    {
+        return 1;
+    }
+
+    return fibonacci(k - 2) + fibonacci(k - 1);
+}
